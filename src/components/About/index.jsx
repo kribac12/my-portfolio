4f4e96a1f66c1e2b5813faada5e17f8e1ccebc59
kribac12 <89196018+kribac12@users.noreply.github.com>
2024-04-03 13:18:38 +0200

    finished project section, added skills section and contact.

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,8 +1,10 @@
 function About() {
   return (
-    <section id="about" className="max-w-6xl mx-auto px-4 py-10 bg-accentColor text-white">
-      {" "}
-      <h2 className="text-3xl font-bold mb-6">About Me</h2>
+    <section id="about" className="px-4 py-10 mt-4 bg-lightColor text-black">
+      <div className="max-w-6xl mx-auto">
+        {" "}
+        <h2 className="uppercase text-4xl font-light mb-6">About me</h2>
+      </div>
     </section>
   );
 }
--- src/components/ProjectCard/index.jsx
+++ src/components/ProjectCard/index.jsx
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 
 function ProjectCard({ title, description, imageUrl, demoUrl, repoUrl }) {
   return (
-    <div className="bg-gray-100 p-4 rounded-lg shadow space-y-4">
+    <div className="bg-white p-4 rounded-lg shadow space-y-4">
       <img
         src={imageUrl}
         alt={title}
@@ -17,15 +17,15 @@ function ProjectCard({ title, description, imageUrl, demoUrl, repoUrl }) {
           href={demoUrl}
           target="_blank"
           rel="noopener noreferrer"
-          className="inline-block bg-gray-500 text-white px-4 py-2 rounded hover:bg-gray-700 transition-colors duration-150 ease-in-out">
+          className="inline-block bg-accentColor text-white px-4 py-2 rounded hover:bg-accentColorDark transition-colors duration-150 ease-in-out">
           Live Demo
         </a>
-        {" | "}
+        {"  "}
         <a
           href={repoUrl}
           target="_blank"
           rel="noopener noreferrer"
-          className="inline-block bg-gray-500 text-white px-4 py-2 rounded hover:bg-gray-700 transition-colors duration-150 ease-in-out ml-2">
+          className="inline-block bg-lightColor text-black px-4 py-2 rounded hover:bg-lightColorDark transition-colors duration-150 ease-in-out ml-2">
           GitHub Repo
         </a>
       </div>
